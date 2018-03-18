a1fe1dd2de97d8c7cf501af98d98f1fd644865fd
kpeck71 <kpeck71@gmail.com>
2018-03-18 19:33:48 -0400

    ingredient_attributes saved to recipe but not appearing on show page. troubleshooting

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -7,9 +7,16 @@ class RecipesController < ApplicationController
 
   def create
     @recipe = Recipe.new(recipe_params)
+    if params[:recipe][:categories]
+      category = Category.find_or_create_by(name: params[:recipe][:categories][:name])
+      @recipe.categories << category
+    end
     if @recipe.save
+      #raise @recipe.inspect
       redirect_to @recipe
       #redirect_to recipe_path(@recipe)
+    else
+      raise @recipe.errors
     end
   end
 
@@ -19,7 +26,7 @@ class RecipesController < ApplicationController
 
   private
   def recipe_params
-    params.require(:recipe).permit(:name, :user_id, :category_ids => [], :ingredients_attributes =>[:name, :description])
+    params.require(:recipe).permit(:name, :user_id, :category_ids => [], ingredients_attributes: [:name, :description])
   end
 
 end
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -6,7 +6,6 @@ class Recipe < ApplicationRecord
   has_many :categories, through: :category_recipes
 
   def ingredients_attributes=(ingredients_attributes)
-    raise ingredients_attributes.inspect
     ingredients_attributes.values.each do |ingredient_attribute|
        ingredient = Ingredient.find_or_create_by(ingredient_attribute)
        self.ingredients.build(ingredient_attribute)
