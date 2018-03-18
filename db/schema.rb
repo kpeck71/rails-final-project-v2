bed886b0047fa71db651120f7dcda64a05b5a16b
kpeck71 <kpeck71@gmail.com>
2018-03-18 19:44:34 -0400

    add instructions to recipes in model and in new recipe form

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180318201532) do
+ActiveRecord::Schema.define(version: 20180318234030) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
@@ -34,6 +34,7 @@ ActiveRecord::Schema.define(version: 20180318201532) do
   create_table "recipes", force: :cascade do |t|
     t.string "name"
     t.integer "user_id"
+    t.text "instructions"
   end
 
   create_table "users", force: :cascade do |t|
