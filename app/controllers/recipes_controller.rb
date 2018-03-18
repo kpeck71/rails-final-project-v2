527c948b701e82ccea463b0e9f6f741ca5e4fca9
kpeck71 <kpeck71@gmail.com>
2018-03-18 19:14:35 -0400

    add show view, action

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -2,6 +2,24 @@ class RecipesController < ApplicationController
 
   def new
     @recipe = Recipe.new
-    3.times{@recipe.ingredients.build}
+    5.times{@recipe.ingredients.build}
   end
+
+  def create
+    @recipe = Recipe.new(recipe_params)
+    if @recipe.save
+      redirect_to @recipe
+      #redirect_to recipe_path(@recipe)
+    end
+  end
+
+  def show
+    @recipe = Recipe.find(params[:id])
+  end
+
+  private
+  def recipe_params
+    params.require(:recipe).permit(:name, :user_id, :category_ids => [], :ingredients_attributes =>[:name, :description])
+  end
+
 end
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -4,4 +4,12 @@ class Recipe < ApplicationRecord
   has_many :ingredients, through: :recipe_ingredients
   has_many :category_recipes
   has_many :categories, through: :category_recipes
+
+  def ingredients_attributes=(ingredients_attributes)
+    raise ingredients_attributes.inspect
+    ingredients_attributes.values.each do |ingredient_attribute|
+       ingredient = Ingredient.find_or_create_by(ingredient_attribute)
+       self.ingredients.build(ingredient_attribute)
+    end
+  end
 end
