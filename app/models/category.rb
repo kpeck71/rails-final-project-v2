5e1d001fd1e4349951348599a3ba7f8e5b2f84a3
kpeck71 <kpeck71@gmail.com>
2018-03-21 18:07:06 -0400

    working on scope method for most_popular categories

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,12 @@
 class Category < ApplicationRecord
   has_many :category_recipes
   has_many :recipes, through: :category_recipes
+
+  def most_popular
+    scope :most_popular, -> { joins(:category_recipes).where('category_id.count.maximum') }
+    # SELECT  "category_id".* FROM "category_recipes" ORDER BY "users"."id" DESC LIMIT ?
+    #users.project(users[:age].count)
+    # => SELECT COUNT(users.age) FROM users
+    
+  end
 end
