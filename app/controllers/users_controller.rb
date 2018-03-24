99d74031e2e38744404606403b4107f8a4d5a6d7
kpeck71 <kpeck71@gmail.com>
2018-03-24 10:12:09 -0400

    add create method and user_params to userscontroller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,5 +3,19 @@ class UsersController < ApplicationController
   end
 
   def create
+    @user = User.create(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to root_path
+      #is that where we should send new users? recipe index?
+    else
+      redirect_to new_user_path
+    end
+  end
+
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :password, :password_confirmation)
   end
 end
