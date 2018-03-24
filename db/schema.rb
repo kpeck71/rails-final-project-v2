e68cbd3683fac7b8f445a4a402ec8119c48d2f81
kpeck71 <kpeck71@gmail.com>
2018-03-24 10:02:46 -0400

    add password digest to users; migrated

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180320001708) do
+ActiveRecord::Schema.define(version: 20180324140209) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
@@ -41,6 +41,7 @@ ActiveRecord::Schema.define(version: 20180320001708) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
 end
