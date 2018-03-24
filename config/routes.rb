3f9c496acac54db6b1af4bed0b923aeca29a0efe
kpeck71 <kpeck71@gmail.com>
2018-03-24 10:09:43 -0400

    update routes to add signup,login,logout

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,21 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
   root to: "recipes#index"
+
+  get 'signup', to: 'users#new', as: 'signup'
+  get 'login', to: 'sessions#new', as: 'login'
+  get 'logout', to: 'sessions#destroy', as: 'logout'
+
+  resources :users
+  resources :sessions
+  resources :recipes
+  resources :categories
+  resources :ingredients
 
   resources :users do
     resources :recipes
   end
 
-  resources :recipes
-  resources :categories
-  resources :ingredients
-
 end
+
+
+# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
