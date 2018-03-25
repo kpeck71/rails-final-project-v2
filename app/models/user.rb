c97691ffc3fbcf85e3840fc2623e3b0066498097
kpeck71 <kpeck71@gmail.com>
2018-03-25 14:28:34 -0400

    update user model for logins

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,22 @@
 class User < ApplicationRecord
   has_secure_password
   has_many :recipes
-  validates_uniqueness_of :email
 
+  validates_presence_of :name, :email, if: :regular_login
+  validates_uniqueness_of :email, if: :regular_login
+  validates_uniqueness_of :name
+
+  def regular_login
+    !self.uid
+  end
+
+  def self.from_omniauth(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_initialize.tap do |user|
+      user.provider = auth.provider
+      user.uid = auth.uid
+      user.name = auth.info.name
+      user.password = SecureRandom.urlsafe_base64(n=6) #needed to generate random password to override "password can't be blank" error
+      user.save!
+    end
+  end
 end
