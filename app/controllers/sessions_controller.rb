49454768b3663c20dec299288e3954d6f91b3bdb
kpeck71 <kpeck71@gmail.com>
2018-04-03 14:38:02 -0400

    continue building Google omniauth login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -20,6 +20,12 @@ class SessionsController < ApplicationController
    end
 
    def create_with_google
+     @user = User.find_or_create_by(uid: auth['uid']) do |u|
+        u.name = auth['info']['name']
+        u.email = auth['info']['email']
+      end
+
+      session[:user_id] = @user.id
    end
 
   def create
