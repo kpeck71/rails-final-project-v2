16187a4ef596364ffb05a1f0ff058fa0ae4827b1
kpeck71 <kpeck71@gmail.com>
2018-05-15 20:55:13 -0400

    remove all facebook omniauth details

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,47 +1,27 @@
 class SessionsController < ApplicationController
   def new
-
   end
 
-  def create_with_facebook
-   user = User.find_or_create_by(:uid => auth['uid']) do |user|
-     user.name = auth['info']['name']
-   end
-     session[:user_id] = user.try(:id)
-  end
+  def create_with_google
+    if @user = User.find_or_create_by(uid: auth['uid']) do |u|
+       u.name = auth['info']['name']
+       u.email = auth['info']['email']
+       end
+       session[:user_id] = @user.id
+       raise @user.inspect
+       redirect_to root_path
+     else
+       flash[:error] = "User not correct or not found GOOGLE"
+       redirect_to root_path
+     end
 
-  def auth
-    request.env['omniauth.auth']
-  end
-   # @user = User.from_omniauth(auth)
-   #   if @user
-   #     session[:user_id] = @user.id
-   #     redirect_to user_path(@user)
-   #   @user = User.find_or_create_by(uid: auth['uid']) do |u|
-   #      u.name = auth['info']['name']
-   #      u.email = auth['info']['email']
-   #   end
-   # end
-   #
-   #  session[:user_id] = @user.id
-   #   # else
-   #   #   redirect_to '/login', flash[:notice] = "Oops, something went wrong here. Please try again."
-   #   # end
-   #end
+    # @user = User.from_omniauth(auth)
+    #   if @user
+    #     session[:user_id] = @user.id
+    #     redirect_to user_path(@user)
+    #   end
 
-   # def create_with_google
-   #   @user = User.from_omniauth(auth)
-   #     if @user
-   #       session[:user_id] = @user.id
-   #       redirect_to user_path(@user)
-   #     end
-   #   # @user = User.find_or_create_by(uid: auth['uid']) do |u|
-   #   #    u.name = auth['info']['name']
-   #   #    u.email = auth['info']['email']
-   #   #  end
-   #
-   #    #session[:user_id] = @user.id
-   # end
+  end
 
   def create
     user = User.find_by(email: params[:email])
@@ -63,7 +43,7 @@ class SessionsController < ApplicationController
 private
 
   def auth
-    request.env['omniauth.auth']
+    request.env["omniauth.auth"]
   end
 
 end
