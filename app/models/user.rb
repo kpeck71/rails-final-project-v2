35f645080c464fcf7ae66b3d179c5cde111e125f
kpeck71 <kpeck71@gmail.com>
2018-05-15 20:54:40 -0400

    remove self.from_omniauth

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,15 +11,4 @@ class User < ApplicationRecord
   def regular_login
     !self.uid
   end
-
-  def self.from_omniauth(auth)
-    raise auth.inspect
-    where(provider: auth.provider, uid: auth.uid).first_or_initialize.tap do |user|
-      user.provider = auth.provider
-      user.uid = auth.uid
-      user.name = auth.info.name
-      user.password = SecureRandom.urlsafe_base64(n=6) # generate random password to override blank password error
-      user.save!
-    end
-  end
 end
