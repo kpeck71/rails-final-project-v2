eb921335fe50e9e91343d26ed7bea852a681e071
kpeck71 <kpeck71@gmail.com>
2018-05-28 10:26:55 -0400

    add only: [:create, :destroy] to sessions route

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   get 'logout', to: 'sessions#destroy', as: 'logout'
 
   resources :users
-  resources :sessions
+  resources :sessions, only: [:create, :destroy]
   resources :recipes #, only: [:new, :create, :update, :destroy]
   resources :categories do
     collection do
