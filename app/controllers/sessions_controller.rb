c0bab5d6c7db9e390b9eecd1bcfd6ff2cb0eb5bf
kpeck71 <kpeck71@gmail.com>
2018-05-28 10:28:18 -0400

    update #create_with_google based on google's omniauth recommendations

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,14 +3,24 @@ class SessionsController < ApplicationController
   end
 
   def create_with_google
-    if @user = User.google_login
-       @user.save
-       session[:user_id] = @user.id
-       redirect_to user_path(@user)
-    else
-       flash[:error] = "User not correct or not found GOOGLE"
-       redirect_to root_path
-     end
+    user_info = request.env["omniauth.auth"]
+
+    user           = User.new
+    user.id        = user_info["uid"]
+    user.name      = user_info["info"]["name"]
+    user.image_url = user_info["info"]["image"]
+
+    session[:user] = Marshal.dump user
+
+    redirect_to root_path
+    # if @user = User.google_login
+    #    @user.save
+    #    session[:user_id] = @user.id
+    #    redirect_to user_path(@user)
+    # else
+    #    flash[:error] = "User not correct or not found GOOGLE"
+    #    redirect_to root_path
+    #  end
   end
 
   def create
