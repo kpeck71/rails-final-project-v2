6571bc5ad9a7a340de3cc190550bc9ae66dd45f0
kpeck71 <kpeck71@gmail.com>
2018-05-28 14:53:05 -0400

    added uniqueness validation for category name

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,6 +2,7 @@ class Category < ApplicationRecord
   has_many :category_recipes
   has_many :recipes, through: :category_recipes
   validates :name, presence: true
+  validates_uniqueness_of :name
 
    scope :sort_by_popularity, -> { order("categories.recipes_count DESC") }
    scope :sort_ABC, -> { order("NAME ASC")}
