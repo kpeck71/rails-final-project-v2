9d9dd61f419d5c209b5ec11cac0b61d46355727a
kpeck71 <kpeck71@gmail.com>
2018-05-28 14:23:05 -0400

    go back to previous current_user method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,13 +3,16 @@ class ApplicationController < ActionController::Base
 
   helper_method :logged_in?, :current_user
 
-   def logged_in?
-     session.has_key? :user
-   end
+  def logged_in?
+    session.has_key? :user
+  end
 
-   def current_user
-     Marshal.load session[:user] if logged_in?
-   end
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+   # def current_user
+   #   Marshal.load session[:user_id] if logged_in?
+   # end
    # def authorize
    #   redirect_to login_url, alert: "Not authorized" if current_user.nil?
    # end
