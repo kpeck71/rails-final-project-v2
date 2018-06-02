d0f312d28adf28407f24c98471b249cc7b441359
kpeck71 <kpeck71@gmail.com>
2018-06-02 09:02:37 -0400

    fixed sort_by_popularity with correct SQL statement

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,20 +1,14 @@
 class Category < ApplicationRecord
   has_many :category_recipes
-  #has_many :recipes, through: :category_recipes
-  has_many :recipes, -> {order 'recipes.count'}, through: :category_recipes
+  has_many :recipes, through: :category_recipes
   validates :name, presence: true
   validates_uniqueness_of :name
 
-   # scope :sort_by_popularity, -> { order("categories.recipes_count DESC") }
+  scope :sort_by_popularity, -> { joins(:recipes)
+    .select("categories.*", "COUNT(recipes.id) AS recipes_count")
+    .group(:category_id)
+    .order("recipes_count DESC")
+  }
    scope :sort_ABC, -> { order("NAME ASC")}
-
-
-  def recipes_count
-    self.recipes.count
-  end
-
-#   has_many :users, -> { order 'users.last_name, users.first_name' }, :through => :assignments
-#   has_many :recipes, -> { order 'recipes' }, :through => :category_recipes
-
 
 end
