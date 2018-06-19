6f5dd263a93e80aa81aae07a3430e6deff439ad7
kpeck71 <kpeck71@gmail.com>
2018-06-19 08:06:44 -0400

    add edit, create, update methods

--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -11,6 +11,23 @@ class IngredientsController < ApplicationController
 
   def edit
     @recipe_ingredient = RecipeIngredient.find_or_create_by(ingredient_id: params[:id], recipe_id: params[:recipe_id])
+    @recipe = params[:recipe_id]
+    # @recipe_ingredient.recipe.id
+    @ingredient = params[:id]
+  end
+
+  def create
+    @recipe_ingredient = RecipeIngredient.find_or_create_by(ingredient_id: params[:id], recipe_id: params[:recipe_id])
+    @recipe = params[:recipe_id]
+    # @recipe_ingredient.recipe.id
+    @ingredient = params[:id]
+  end
+
+  def update
+    @recipe_ingredient = RecipeIngredient.find_or_create_by(ingredient_id: params[:id], recipe_id: params[:recipe_id])
+    @recipe_ingredient.notes = params[:recipe_ingredient][:notes]
+    @recipe_ingredient.save
+    redirect_to edit_recipe_path(@recipe_ingredient.recipe.id)
   end
 
   def sort_ABC
