2e1d93c6d751dc1f97d45233f15adc916a70a2fd
kpeck71 <kpeck71@gmail.com>
2018-06-24 16:43:51 -0400

    remove unnecessary code

--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -9,27 +9,6 @@ class IngredientsController < ApplicationController
     render :index
   end
 
-  def edit
-    @recipe_ingredient = RecipeIngredient.find_or_create_by(ingredient_id: params[:id], recipe_id: params[:recipe_id])
-    @recipe = params[:recipe_id]
-    # @recipe_ingredient.recipe.id
-    @ingredient = params[:id]
-  end
-
-  def create
-    @recipe_ingredient = RecipeIngredient.find_or_create_by(ingredient_id: params[:id], recipe_id: params[:recipe_id])
-    @recipe = params[:recipe_id]
-    # @recipe_ingredient.recipe.id
-    @ingredient = params[:id]
-  end
-
-  def update
-    @recipe_ingredient = RecipeIngredient.find_or_create_by(ingredient_id: params[:id], recipe_id: params[:recipe_id])
-    @recipe_ingredient.notes = params[:recipe_ingredient][:notes]
-    @recipe_ingredient.save
-    redirect_to edit_recipe_path(@recipe_ingredient.recipe.id)
-  end
-
   def sort_ABC
     @ingredients = Ingredient.sort_ABC
     render :index
