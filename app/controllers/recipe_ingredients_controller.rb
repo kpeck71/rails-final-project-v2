4ec2e65263c57b00469d7ca3fbc271a10a4f31c4
kpeck71 <kpeck71@gmail.com>
2018-06-27 08:42:29 -0400

    create recipe_ing cotroller

---
+++ app/controllers/recipe_ingredients_controller.rb
@@ -0,0 +1,34 @@
+class RecipeIngredientsController < ApplicationController
+  before_action :set_recipe_ingredient, only: [:index, :show, :edit, :update]
+
+  def index
+    @recipe = @recipe_ingredient.recipe
+  end
+
+  def show
+    @recipe_ingredient = RecipeIngredient.find(params[:id])
+  end
+
+  # def edit
+  #   @recipe = @recipe_ingredient.recipe
+  #   @ingredient = @recipe_ingredient.ingredient
+  # end
+
+  def update
+    @recipe_ingredient.update(recipe_ingredient_params)
+    if @recipe_ingredient.save
+      redirect_to recipe_path(@recipe_ingredient.recipe)
+    end
+
+  end
+
+  private
+
+  def set_recipe_ingredient
+    @recipe_ingredient = RecipeIngredient.find(params[:id])
+  end
+
+  def recipe_ingredient_params
+    params.require(:recipe_ingredient).permit(:notes)
+  end
+end
