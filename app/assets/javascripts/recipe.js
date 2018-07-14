3af9e21b4109553ffe2513af07d060d0a1061628
kpeck71 <kpeck71@gmail.com>
2018-07-14 18:50:38 -0400

    added category section to recipe show page

--- app/assets/javascripts/recipe.js
+++ app/assets/javascripts/recipe.js
@@ -1,4 +1,5 @@
-function loadRecipes(data) {
+$(function () {
+  function loadRecipes(data) {
   $(".card-header").text(data["name"])
   $(".ingredientsList").text("");
   data["ingredients"].forEach(function(ingredient) {
@@ -7,12 +8,15 @@ function loadRecipes(data) {
     $(".ingredientsList").append(`<li><a href="/ingredients/${id}">${name}</a></li>`);
   })
   $(".recipeInstructions").text(data["instructions"]);
+  var categories = data["categories"]
+  categories.forEach(function(category) {
+    $(".recipeCategories").html(`<li><a href="/categories/${category.id}">${category.name}</a></li>`);
+  })
   // re-set the id to current on the link
   $(".js-next").attr("data-id", data["id"]);
   $(".js-prev").attr("data-id", data["id"]);
 }
 
-$(function () {
   $(".js-next").on("click", function() {
     var nextId = parseInt($(".js-next").attr("data-id")) + 1;
     $.get("/recipes/" + nextId + ".json", function(data) {
@@ -29,17 +33,3 @@ $(function () {
     event.preventDefault();
   });
 });
-
-//
-// $(function () {
-//   $(".js-next").on("click", function() {
-//     var nextId = parseInt($(".js-next").attr("data-id")) + 1;
-//     $.get("/recipes/" + nextId + ".json", function(data) {
-//       $(".card-header").text(data["name"]);
-//       $(".recipeIngredients").text(data["ingredients"]);
-//       $(".recipeInstructions").text(data["description"]);
-//       // re-set the id to current on the link
-//       $(".js-next").attr("data-id", data["id"]);
-//     });
-//   });
-// });
