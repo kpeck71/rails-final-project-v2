d9befbd50e6e18654d3d9b22b6079a42e7b9e40b
kpeck71 <kpeck71@gmail.com>
2018-07-14 17:05:48 -0400

    toggle the add new category form on category index page

--- app/assets/javascripts/categories.js
+++ app/assets/javascripts/categories.js
@@ -1,58 +1,63 @@
 $(function () {
 
-function Recipe(recipe) {
-  this.name = recipe.name
-  this.id = recipe.id
-}
+  function Recipe(recipe) {
+    this.name = recipe.name
+    this.id = recipe.id
+  }
 
-Recipe.prototype.formatRecipe = function() {
-  var recipeHTML = `<p><a href="/recipes/${this.id}">${this.name}</a></p>`
-  return recipeHTML
-}
+  Recipe.prototype.formatRecipe = function() {
+    var recipeHTML = `<p><a href="/recipes/${this.id}">${this.name}</a></p>`
+    return recipeHTML
+  }
 
-function Category(category) {
-  this.id = category.id
-  this.name = category.name
-}
+  function Category(category) {
+    this.id = category.id
+    this.name = category.name
+  }
 
-Category.prototype.renderCategory = function() {
-  // var categoryHTML = `<a href="#" class="load-recipes"><%= ${this.name} %></a>`
-  var categoryHTML = `<li class="list-group-item d-flex justify-content-between align-items-center">
-        <a href="#" class="load-recipes" id="${this.id}">${this.name}</a>
-        <span class="badge badge-primary badge-pill">0 recipes</span>
-      </li>`
-  return categoryHTML
-}
+  Category.prototype.renderCategory = function() {
+    // var categoryHTML = `<a href="#" class="load-recipes"><%= ${this.name} %></a>`
+    var categoryHTML = `<li class="list-group-item d-flex justify-content-between align-items-center">
+          <a href="#" class="load-recipes" id="${this.id}">${this.name}</a>
+          <span class="badge badge-primary badge-pill">0 recipes</span>
+        </li>`
+    return categoryHTML
+  }
 
-$("form#new_category").on("submit", function() {
-  event.preventDefault();
-  var $form = $(this);
-  var action = $form.attr("action");
-  var params = $form.serialize();
-  $.ajax({
-    url: action,
-    data: params,
-    dataType: "json",
-    method: "POST"
-  }).success(function(json) {
-    var category = new Category(json);
-    var renderCategory = category.renderCategory();
-    $("ul.list-group").append(renderCategory);
+  $("form#new_category").on("submit", function() {
+    event.preventDefault();
+    var $form = $(this);
+    var action = $form.attr("action");
+    var params = $form.serialize();
+    $.ajax({
+      url: action,
+      data: params,
+      dataType: "json",
+      method: "POST"
+    }).success(function(json) {
+      var category = new Category(json);
+      var renderCategory = category.renderCategory();
+      $("ul.list-group").append(renderCategory);
+    });
   });
-});
 
-$("a.load-recipes").on("click", function() {
-  var div = $(".recipes-container")
-  var categoryId = this.id
+  $("a.load-recipes").on("click", function() {
+    var div = $(".recipes-container")
+    var categoryId = this.id
 
-  $.get("/categories/" + categoryId + ".json").success(function(recipes) {
-    div.html("")
-    recipes.forEach(function(recipe) {
-      var oneRecipe = new Recipe(recipe)
-      var recipeHTML = oneRecipe.formatRecipe()
-      div.append(recipeHTML)
-    })
+    $.get("/categories/" + categoryId + ".json").success(function(recipes) {
+      div.html("")
+      recipes.forEach(function(recipe) {
+        var oneRecipe = new Recipe(recipe)
+        var recipeHTML = oneRecipe.formatRecipe()
+        div.append(recipeHTML)
+      })
+    });
+    event.preventDefault();
   });
-  event.preventDefault();
-});
+
+  $(".add-new-category").on("click", function() {
+      $("#new-category-container").toggle();
+    });
+    event.preventDefault();
 })
